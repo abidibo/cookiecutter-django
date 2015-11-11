f20bc41ef37c6c051a005de862b9e95c3ae915e5
abidibo <abidibo@gmail.com>
2015-11-11 11:48:56 +0100

    fixed gitignore

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,15 +3,17 @@
 ##
 # Does the following:
 #
-# 1 - Installs system required packages
-# 2 - Creates a new database
-# 3 - Creates a virtualenv
-# 4 - Installs app requirements
-# 5 - Db initial migration
-# 6 - Repository initialization
-# 7 - Installs sass gem
-# 8 - Activates the created virtualenv
+# 1 - Enables gitignore
+# 2 - Installs system required packages
+# 3 - Creates a new database
+# 4 - Creates a virtualenv
+# 5 - Installs app requirements
+# 6 - Db initial migration
+# 7 - Repository initialization
+# 8 - Installs sass gem
+# 9 - Activates the created virtualenv
 ##
 
+mv gitignore .gitignore
 bin/ansible_local
 source .virtualenv/bin/activate
