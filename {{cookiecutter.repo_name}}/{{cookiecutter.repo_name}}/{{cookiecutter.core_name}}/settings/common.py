3d26183d2eef58b65ea8d7a9c97f48d3f812d890
abidibo <abidibo@gmail.com>
2016-07-20 14:59:09 +0200

    added DEBUG to context processors

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.core_name}}/settings/common.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.core_name}}/settings/common.py
@@ -115,6 +115,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.debug',
             ],
         },
     },
