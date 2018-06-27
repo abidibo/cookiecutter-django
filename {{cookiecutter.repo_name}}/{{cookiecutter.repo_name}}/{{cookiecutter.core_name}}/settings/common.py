9dcedc8f27b30020646f29961ff59ec264c51286
abidibo <abidibo@gmail.com>
2018-06-27 14:22:30 +0200

    added file permissions

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.core_name}}/settings/common.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.core_name}}/settings/common.py
@@ -148,6 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Uploaded files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+FILE_UPLOAD_PERMISSIONS = 0o644
 
 # ADMIN
 {% if cookiecutter.admin == 'django-baton' %}
